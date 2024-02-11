9206472c90ec70a4364d02547f042a859009e882
İREM AYTEN <126502980+iremayten6@users.noreply.github.com>
2024-02-11 18:39:47 +0300

    first commit

---
+++ Workaround/Program.cs
@@ -0,0 +1,43 @@
+﻿using Business.Concrete;
+using Entities.Concrete;
+using System;
+
+
+namespace Workaround
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // arrays
+            string[] ogrenciler = new string[3];
+            ogrenciler[0] = "Engin";
+            ogrenciler[1] = "Kerem";
+            ogrenciler[2] = "Berkay";
+
+            for(int i = 0; i < ogrenciler.Length; i++)
+            {
+                Console.WriteLine(ogrenciler[i]);
+            }
+            List<string> yeniSehirler = new List<string> { "Ankara", "İstanbul", "Bursa" };
+            yeniSehirler.Add("Adana");
+            foreach(var sehir in yeniSehirler)
+            {
+                Console.WriteLine(sehir);
+            }
+            Person person = new Person();
+            person.FirstName = "Engin";
+
+            PttManager pttManager = new PttManager(new ForeignerManager());
+            pttManager.GiveMask(new Person());
+            
+            Console.ReadKey();
+        }
+
+        static void SelamVer()
+        {
+            Console.WriteLine("Merhaba");
+        }
+    }
+ 
+}
